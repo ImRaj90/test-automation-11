367190cea49cf796a9f3e7ded692afe5c34efd88
vicky <swamyrajashekar@gmail.com>
2024-09-19 13:18:52 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,6 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
